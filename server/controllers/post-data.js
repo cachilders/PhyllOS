1c7723a9e158cf64dbb4d50e67b4f5fd34e2f89b
cachilders <cachilders@gmail.com>
2016-09-21 19:22:40 -0700

    [bug in] On device edits

--- server/controllers/post-data.js
+++ server/controllers/post-data.js
@@ -6,13 +6,13 @@ var request = require('request');
 
 var httpRequestOptions = {
   // CHANGE this line to API endpoint
-  url: 'process.env.POST_TO_ADDRESS',
+  url: 'http://phyll-dev.herokuapp.com/io/record'
 };
 
 router.post('/', function(req, res){
-  httpRequestOptions.sensorData = req.body;
-  console.log('Sending ', req.body.status);
-  request(httpRequestOptions, function(error, response, body){
+  httpRequestOptions.body = {status: req.body.status};
+  console.log('Sending ', req.body);
+  request.post(httpRequestOptions, function(error, response, body){
     console.log("response: ", body);
   })
 })
